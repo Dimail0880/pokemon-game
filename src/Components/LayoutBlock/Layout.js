c96c87d06e7ec2f426561201506b6371047ea01b
dimail0880 <dimail4888@gmail.com>
2021-01-28 20:46:31 +0200

    task is not done

--- src/Components/LayoutBlock/Layout.js
+++ src/Components/LayoutBlock/Layout.js
@@ -1,22 +1,24 @@
 import React, { Component } from "react";
 import styles from "./Layout.module.css";
 
-export default class Layout extends Component {
-  render() {
-    return (
-      <>
-        <section className={styles.root} id="<-- ЗДЕСЬ props.id -->">
-          <div className={styles.wrapper}>
-            <article>
-              <div className={styles.title}>
-                {/* <h3>{props.title}</h3> */}
-                <span className={styles.separator}></span>
-              </div>
-              <div className={styles.desc}>{/* <p>{props.desc}</p> */}</div>
-            </article>
-          </div>
-        </section>
-      </>
-    );
-  }
-}
+const Layout = (props) => {
+  return (
+    <>
+      <section className={styles.root} id={props.id}>
+        <div className={styles.wrapper}>
+          <article>
+            <div className={styles.title}>
+              <h3>{props.title}</h3>
+              <span className={styles.separator}></span>
+            </div>
+            <div className={styles.desc}>
+              <p>{props.desc}</p>
+            </div>
+          </article>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Layout;
